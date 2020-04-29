8e3e728a5ba9e700bd3e6d645ecf02d51e5b2fac
Martin <martinwjwilson@gmail.com>
2020-04-29 14:58:10 +0100

    created method to retrieve user information for creating a profile

--- cogs/profile.py
+++ cogs/profile.py
@@ -8,21 +8,29 @@ class Profile(commands.Cog):
         self.bot = bot
 
 
-
-    # copies the senders message to the specified channel
-    @commands.command()
-    async def createprofile(self, ctx):
+    # ask a user for their name
+    async def get_info(self, ctx, question):
         def dm_check(m):
             return m.channel.type == discord.channel.ChannelType.private and m.author == ctx.author
-
-        await ctx.author.send("What is your villager's name?")
+        await ctx.author.send(question) # send the user a dm
         try:
-            await self.bot.wait_for('message', check = dm_check, timeout = 10.0)
+            recieved_message = await self.bot.wait_for('message', check = dm_check, timeout = 10.0) # wait for a response
+            message_content = recieved_message.content
         except asyncio.TimeoutError:
-            await ctx.author.send("The message timed out")
+            await ctx.author.send("The message timed out") # if they do not respond
         else:
-            await ctx.author.send("You replied to the message")
+            return message_content
 
+    # create a profile for the user
+    @commands.command()
+    async def createprofile(self, ctx):
+        # obtain the user information
+        villager_name = await self.get_info(ctx, "What is your villager's name?")
+        print(villager_name)
+        # discord_name =
+        island_name = await self.get_info(ctx, "What is your island's name?")
+        print(island_name)
+        # friend_code =
 
 def setup(bot):
     bot.add_cog(Profile(bot))
