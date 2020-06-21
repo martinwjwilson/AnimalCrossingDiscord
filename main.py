dddec3e56b51a82fa48a9b4a75d172fcfdd81072
Martin <martinwjwilson@gmail.com>
2020-06-21 13:05:31 +0100

    finished help command info

--- main.py
+++ main.py
@@ -27,21 +27,30 @@ async def help(ctx, command_name: typing.Optional[str] = ""):
     """
     if command_name == "":
         embed = discord.Embed(title = "Command List", description = "Here are all of the bot commands!\nFor information on using a specific command, use `>help {command}`")
-        embed.add_field(name = "Search:", value = "s\nfish\nbug\nmonth\narriving\nleaving", inline = False)
+        embed.add_field(name = "Search:", value = "s\nfish\nbug\nmonth\narriving\nleaving")
     elif command_name == "s":
         embed = discord.Embed(title = ">s critter_name", description = "Search for a critter by name and display all related information")
-        embed.add_field(name = "Example", value = "`>s bitterling` would return all of the details about the bitterling such as size, value, etc.", inline = False)
+        embed.add_field(name = "Example", value = "`>s bitterling` will return all of the details about the bitterling such as size, value, etc")
     elif command_name == "fish":
-        embed = discord.Embed(title = "null:", description = "null")
+        embed = discord.Embed(title = ">fish starts_with", description = "Display a list of all fish in alphabetical order. If the optional `starts_with` is provided then the list will show fish starting with the input")
+        embed.add_field(name = "Example 1", value = "`>fish` will return all fish in the game in alphabetical order")
+        embed.add_field(name = "Example 2", value = "`>fish se` will return:\n- Sea Bass\n- Sea Butterfly\n- Sea Horse", inline = False)
     elif command_name == "bug":
-        embed = discord.Embed(title = "null:", description = "null")
+        embed = discord.Embed(title = ">bug starts_with", description = "Display a list of all bugs in alphabetical order. If the optional `starts_with` is provided then the list will show bugs starting with the input")
+        embed.add_field(name = "Example 1", value = "`>bug` will return all bugs in the game in alphabetical order")
+        embed.add_field(name = "Example 2", value = "`>bug sc` will return:\n- Scarab Beetle\n- Scorpion", inline = False)
     elif command_name == "month":
-        embed = discord.Embed(title = "null:", description = "null")
+        embed = discord.Embed(title = ">month", description = "Get a list of all fish and bugs available this month")
+        embed.add_field(name = "Notes:", value = "Currently this command only works for the northern hemisphere")
     elif command_name == "arriving":
-        embed = discord.Embed(title = "null:", description = "null")
+        embed = discord.Embed(title = ">arriving hemisphere", description = "Display a list of all fish and bugs arriving in the current month. The hemisphere is optional, `n` for northern and `s` for southern. The default hemisphere is `n`")
+        embed.add_field(name = "Example 1", value = "`>arriving` will rdisplay a list of all the fish and bugs arriving in the current month in the northern hemisphere")
+        embed.add_field(name = "Example 2", value = "`>arriving s` will rdisplay a list of all the fish and bugs arriving in the current month in the southern hemisphere")
     elif command_name == "leaving":
-        embed = discord.Embed(title = "null:", description = "null")
-    embed.add_field(name = "Support the bot!", value = "*{inset_link_here}*", inline = False)
+        embed = discord.Embed(title = ">leaving hemisphere", description = "Display a list of all fish and bugs leaving in the current month. The hemisphere is optional, `n` for northern and `s` for southern. The default hemisphere is `n`")
+        embed.add_field(name = "Example 1", value = "`>leaving` will rdisplay a list of all the fish and bugs leaving in the current month in the northern hemisphere")
+        embed.add_field(name = "Example 2", value = "`>leaving s` will rdisplay a list of all the fish and bugs leaving in the current month in the southern hemisphere")
+    embed.add_field(name = "Support the bot!", value = "*https://ko-fi.com/plugs*", inline = False)
     await ctx.send(embed = embed)
 
 
