74c680590b1a4d27bdf2a3548dfecd96e24d5f0b
Martin <martinwjwilson@gmail.com>
2020-08-21 16:37:05 +0100

    renamed variables in copied section from fish to bug critter conversion

--- scripts/db/fish_migration.py
+++ scripts/db/fish_migration.py
@@ -112,7 +112,7 @@ for fish in fish_list:
 
     # convert the fish properties ready to fit critter class
     fish_name = fish[0]
-    fisih_species = fish[1]
+    fish_species = fish[1]
     fish_location = fish[2]
     fish_size = fish[3]
     fish_value = fish[4]
@@ -151,13 +151,13 @@ for fish in fish_list:
     insert_critter_into_db(critter)
 
 # convert each bug into a critter
-for fish in fish_list:
+for bug in bug_list:
     # get the time periods
-    fish_time = fish[5]
-    if fish_time == "All day":
-        fish_time = "12 a.m. - 12 a.m."
+    bug_time = bug[5]
+    if bug_time == "All day":
+        bug_time = "12 a.m. - 12 a.m."
     # fix case for 2 time periods
-    split_time = fish_time.split(",") # check for 2 time periods
+    split_time = bug_time.split(",") # check for 2 time periods
     temp_list = []
     for time_period in split_time:
         time_list = get_time_start_end(time_period)
@@ -166,27 +166,27 @@ for fish in fish_list:
             temp_list.append(time)
 
     # get the month periods
-    fish_dates = fish[6]
+    bug_dates = bug[6]
     start_date_second = "null"
     end_date_second = "null"
 
-    if "round" in fish_dates:
+    if "round" in bug_dates:
         start_date = "All Year"
         end_date = "All Year"
     else:
         # get the northern hemisphere months
-        fish_dates = fish_dates.split("/")[0].strip().replace(" (Northern)", "")
+        bug_dates = bug_dates.split("/")[0].strip().replace(" (Northern)", "")
         # check if only one month
-        if "-" not in fish_dates: # start and end month are the same
-            start_date = fish_dates
-            end_date = fish_dates
+        if "-" not in bug_dates: # start and end month are the same
+            start_date = bug_dates
+            end_date = bug_dates
         # check if only one period
-        elif "," not in fish_dates:
-            both_dates = get_month_start_end(fish_dates)
+    elif "," not in bug_dates:
+            both_dates = get_month_start_end(bug_dates)
             start_date = both_dates[0]
             end_date = both_dates[1]
         else:
-            both_periods = fish_dates.split(", ")
+            both_periods = bug_dates.split(", ")
             both_dates_first = get_month_start_end(both_periods[0])
             both_dates_second = get_month_start_end(both_periods[1])
             start_date = both_dates_first[0]
@@ -195,41 +195,41 @@ for fish in fish_list:
             end_date_second = both_dates_second[1]
 
     # convert the fish properties ready to fit critter class
-    fish_name = fish[0]
-    fisih_species = fish[1]
-    fish_location = fish[2]
-    fish_size = fish[3]
-    fish_value = fish[4]
-    fish_start_time_period_1 = temp_list[0]
-    fish_end_time_period_1 = temp_list[1]
+    bug_name = bug[0]
+    bug_species = bug[1]
+    bug_location = bug[2]
+    bug_size = bug[3]
+    bug_value = bug[4]
+    bug_start_time_period_1 = temp_list[0]
+    bug_end_time_period_1 = temp_list[1]
     if len(temp_list) == 4:
-        fish_start_time_period_2 = temp_list[2]
-        fish_end_time_period_2 = temp_list[3]
+        bug_start_time_period_2 = temp_list[2]
+        bug_end_time_period_2 = temp_list[3]
     else:
-        fish_start_time_period_2 = None
-        fish_end_time_period_2 = None
-    fish_start_month = start_date
-    fish_end_month = end_date
-    fish_start_month_period_2 = start_date_second
-    fish_end_month_period_2 = end_date_second
-    fish_image_url = fish[7]
+        bug_start_time_period_2 = None
+        bug_end_time_period_2 = None
+    bug_start_month = start_date
+    bug_end_month = end_date
+    bug_start_month_period_2 = start_date_second
+    bug_end_month_period_2 = end_date_second
+    bug_image_url = bug[7]
 
     # convert fish into critter
     critter = Critter(
-        fish_name,
-        fish_size,
-        fish_location,
-        fish_size,
-        fish_value,
-        fish_start_time_period_1,
-        fish_end_time_period_1,
-        fish_start_time_period_2,
-        fish_end_time_period_2,
-        fish_start_month,
-        fish_end_month,
-        fish_start_month_period_2,
-        fish_end_month_period_2,
-        fish_image_url)
+        bug_name,
+        bug_size,
+        bug_location,
+        bug_size,
+        bug_value,
+        bug_start_time_period_1,
+        bug_end_time_period_1,
+        bug_start_time_period_2,
+        bug_end_time_period_2,
+        bug_start_month,
+        bug_end_month,
+        bug_start_month_period_2,
+        bug_end_month_period_2,
+        bug_image_url)
 
     print(critter.start_month_second)
     insert_critter_into_db(critter)
