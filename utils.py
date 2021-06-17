5e6cbebf37db7618e49cfa2e068b25b69716dcc6
Martin <martinwjwilson@gmail.com>
2021-06-17 22:05:40 +0100

    Formatting

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 # check if a developer is using the command
 list_of_developers = {
-"Plugs": 146737110974595073,
-"RedVelvetUnderground": 449997098956488724
+    "Plugs": 146737110974595073,
+    "RedVelvetUnderground": 449997098956488724
 }
+
 
 def check_if_it_is_dev(ctx):
     if ctx.message.author.id in list_of_developers.values():
@@ -14,6 +15,7 @@ def check_for_critter(name):
     return f"""SELECT *
                 FROM critter
                 WHERE critter_name = '{name}'"""
+
 
 def search_all_critters(type, starts_with):
     return f"""SELECT *
