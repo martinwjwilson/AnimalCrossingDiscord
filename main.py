20bbb92e097b29d1d9956f7a96b117f8f249db4b
Martin <martinwjwilson@gmail.com>
2025-04-21 12:51:59 +0100

    User input in main
    Take user input when running the script, allowing for other commands to be chosen in the future

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from search import Search
 
 
 if __name__ == '__main__':
-    search = Search()
-    search.month()
+    while True:
+        user_input = input("Hi there, what would you like to know?")
+        match user_input:
+            case "search":
+                search = Search()
+                search.month()
+            case _:
+                print("Sorry that doesn't match any of my commands\n")
